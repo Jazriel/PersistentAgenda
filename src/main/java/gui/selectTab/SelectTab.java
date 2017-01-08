ca30d29084f48a2c51c574103de4f25907e2be51
JavierMartinezRiberas <jmr0082@alu.ubu.es>
2017-01-08 19:31:16 +0100

    Experimental changes to the project in wait for aproval.(Fixed)

--- src/main/java/gui/selectTab/SelectTab.java
+++ src/main/java/gui/selectTab/SelectTab.java
@@ -1,15 +1,10 @@
 package gui.selectTab;
 
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
 
 import persistence.IFactoryPersistence;
 
@@ -25,14 +20,8 @@ import persistence.IFactoryPersistence;
  */
 public class SelectTab {
 
-	/**
-	 * filOrdStrings.
-	 */
-	private final String[] filOrdStrings = new String[] { "Filtrar", "Ordenar" };
-
 	private Map<Integer, SelectState> viewDict;
 	private JPanel selectPanel;
-	private DefaultComboBoxModel filOrdComboModel;
 
 	/**
 	 * Método SelectTab. Contructor de la clase.
@@ -46,7 +35,6 @@ public class SelectTab {
 		selectPanel = new JPanel();
 		tabbedPane.addTab("Consultar", null, selectPanel, null);
 
-		filOrdComboModel = new DefaultComboBoxModel(filOrdStrings);
 
 		viewDict = new HashMap<>();
 		viewDict.put(0, new SelectContactState(persistence.createContactPersistence()));
@@ -55,18 +43,6 @@ public class SelectTab {
 		selectPanel.add(viewDict.get(0).getView());
 	}
 
-	private void dateComboListener(JComboBox fieldCombo, JTextField filterTextField) {
-		fieldCombo.addItemListener(new ItemListener() {
-
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				filterTextField.setText("");
-				if (fieldCombo.getSelectedIndex() == 1) {
-					filterTextField.setText("dd/mm/yyyy");
-				}
-			}
-		});
-	}
 
 	/**
 	 * Método setView. Método que se encarga de establecer una vista.
